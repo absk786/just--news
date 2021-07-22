2fc874b29a6d1d93573a4fc50785b69c0df54163
Abdul Khalil <rehmank11@gmail.com>
2021-07-22 19:19:05 +0100

    cant get server to work

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
-const User = require('./user')
+const User = require('./User')
 const Post = require('./Post')
+
+//create associations
+//a user can make many posts
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+})
+
+//this means that a post can only belong to one user
+Post.belongsTo(User, {
+    foreignKey:'user_id'
+})
 
 module.exports = {User, Post};
