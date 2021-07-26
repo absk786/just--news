a94b1f1696ad3bf0ffcc0a377ab255795051f4f3
absk786 <rehmank11@gmail.com>
2021-07-26 14:19:00 +0100

    post routes completed

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const {Post, User} = require('../../models')
-
+const {Post, User} = require('../../models');
+const { route } = require('./user-routes');
 
 //get all users 
 router.get('/', (req,res) =>{
@@ -19,8 +19,97 @@ router.get('/', (req,res) =>{
     })
 })
 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
+  router.post('/', (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_url:req.body.post_url,
+        user_id:req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+  })
 
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+//   In the GET route that returns all the posts, let's add the order property to the findAll query by adding the following code:
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    order: [['created_at', 'DESC']], 
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
 module.exports = router;
