d38c44fcb136d91d25344733fbbd9a1640a00a40
absk786 <rehmank11@gmail.com>
2021-07-31 18:04:58 +0100

    updating

--- models/index.js
+++ models/index.js
@@ -1,15 +1,43 @@
-const User = require('./User')
-const Post = require('./Post')
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
 
-//create associations
-//a user can make many posts
-User.hasMany(Post,{
-    foreignKey: 'user_id'
-})
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-//this means that a post can only belong to one user
 Post.belongsTo(User, {
-    foreignKey:'user_id'
-})
+  foreignKey: 'user_id'
+});
 
-module.exports = {User, Post};
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
