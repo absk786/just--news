f3725cab5e35acc24e64b3c39ffc52296ed4339b
absk786 <rehmank11@gmail.com>
2021-08-01 06:56:33 +0100

    started module 14

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const routes = require('./routes')
 const sequelize = require('./config/connection');
 const app = express()
 const PORT = process.env.PORT||3001;
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 //turn on routes
 app.use(routes);
-
+app.use(express.static(path.join(__dirname, 'public')));
 //turn on connection to db and server
 sequelize.sync({force:true}).then(() => {
 app.listen(PORT, () => console.log('now listening'))
