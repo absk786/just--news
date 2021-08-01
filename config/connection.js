8f6eed73f728c4c7e255394a8e0b97ea1bc3c312
absk786 <rehmank11@gmail.com>
2021-08-01 06:36:44 +0100

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,17 @@ const Sequelize = require('sequelize')
 require('dotenv').config()
 // All we need it to do here is execute when we use connection.js and all of the data in the .env file will be made available at process.env.<ENVIRONMENT-VARIABLE-NAME>.
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW, {
-    host:'localhost',
-    dialect:'mysql',
-    port:'3306'
-})
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 module.exports = sequelize;
 
 // All we're doing here is importing the base Sequelize class and using it to create a new connection to the database. 
