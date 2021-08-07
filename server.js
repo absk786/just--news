1d74ef3a292f3b48e3d712bfb4bd6ce7beaa5467
absk786 <rehmank11@gmail.com>
2021-08-07 09:48:23 +0100

    asdf

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
-const routes = require('./routes')
+const routes = require('./controllers')
 const sequelize = require('./config/connection');
 const app = express()
 const PORT = process.env.PORT||3001;
 const path = require('path')
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 //turn on routes
-app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
 //turn on connection to db and server
 sequelize.sync({force:true}).then(() => {
 app.listen(PORT, () => console.log('now listening'))
